4c3a932a88cab4b08af60721eb854ae158a05cc8
Minh chim to <tuan.na@tinasoft.com.vn>
2025-10-03 22:12:52 +0700

    fix:  fix authen auto go to home

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,20 @@
-import React, { PropsWithChildren } from "react";
+"use client";
+
+import React, { PropsWithChildren, useEffect } from "react";
+import { useAuthStore } from "@/store/slices/auth.slice";
+import { Routes } from "@/lib/routes/routes";
+import { useRouter } from "next/navigation";
 
 function AuthLayout({ children }: PropsWithChildren) {
+  const { user } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push(Routes.home);
+    }
+  }, [user]);
+
   return <div>{children}</div>;
 }
 
